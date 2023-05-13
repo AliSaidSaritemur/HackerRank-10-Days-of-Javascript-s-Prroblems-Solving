680f6c8c042cf5c08bbb90c8e856f93bb7a14aee
Said Saritemur <AliSaidSaritemur@gmail.com>
2023-05-14 01:33:00 +0300

    Day 1: Arithmetic Operators's Solving Added

---
+++ Day 1/Arithmetic Operators.js
@@ -0,0 +1,12 @@
+function getArea(length, width) {
+    let area;
+    area=Number(length)*Number(width);
+    return area;
+}
+
+
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter=(Number(length)+Number(width))*2    
+    return perimeter;
+}
