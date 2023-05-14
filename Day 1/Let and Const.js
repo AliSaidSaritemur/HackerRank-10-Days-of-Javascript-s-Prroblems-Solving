7de82af45242ca92b59ee9642ae446f5fa363290
Said Saritemur <AliSaidSaritemur@gmail.com>
2023-05-14 16:03:24 +0300

    Day 1: Let and Const's Solving Added

---
+++ Day 1/Let and Const.js
@@ -0,0 +1,7 @@
+function main() {
+
+const PI=Math.PI;
+let radiusOfCircle = readLine();
+let area=Math.pow(radiusOfCircle,2)*PI;
+let perimeter=2*PI*radiusOfCircle;
+console.log(`${area} \n${perimeter}`);
